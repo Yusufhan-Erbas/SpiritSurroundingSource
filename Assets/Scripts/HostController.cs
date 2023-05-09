23df302a655564e3eed313b3128b73b065c32624
Yusufhan ERBAŞ <63731504+Yusufhan-Erbas@users.noreply.github.com>
2023-05-10 02:37:04 +0300

    Ghost capture host smoothly.

--- Assets/Scripts/HostController.cs
+++ Assets/Scripts/HostController.cs
@@ -6,6 +6,7 @@ public class HostController : MonoBehaviour
 {
 	Rigidbody2D rb;
 	public float speed = 20f;
+	bool isCaptureStart = false;
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
@@ -26,19 +27,28 @@ public class HostController : MonoBehaviour
 		}
 		else if (PlayerController.isCapture)
 		{
-			StartCoroutine(HostCaptured());
+			if (!isCaptureStart)
+			{
+				StartCoroutine(HostCaptured());
+				return;
+			}
+			else if(isCaptureStart)
+			{
+				if (Input.GetKey(KeyCode.D))
+				{
+					transform.Translate(Vector2.right * speed * Time.deltaTime);
+				}
+				else if (Input.GetKey(KeyCode.A))
+				{
+					transform.Translate(Vector2.left * speed * Time.deltaTime);
+				}
+			}
 		}
 		IEnumerator HostCaptured()
 		{
-			yield return new WaitForSeconds(1f);
-			if (Input.GetKey(KeyCode.D))
-			{
-				transform.Translate(Vector2.right * speed * Time.deltaTime);
-			}
-			else if (Input.GetKey(KeyCode.A))
-			{
-				transform.Translate(Vector2.left * speed * Time.deltaTime);
-			}
+			yield return new WaitForSeconds(2f);
+			isCaptureStart = true;
+		
 		}
 	
 	}
