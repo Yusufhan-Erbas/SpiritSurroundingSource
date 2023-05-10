84b6e79d220a60110023280d6087709d07ef92eb
Yusufhan ERBAŞ <63731504+Yusufhan-Erbas@users.noreply.github.com>
2023-05-10 03:42:03 +0300

    Host turn movement function written.

--- Assets/Scripts/HostController.cs
+++ Assets/Scripts/HostController.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class HostController : MonoBehaviour
 {
 	Rigidbody2D rb;
+	Animator hostAnim;
 	public float speed = 20f;
 	bool isCaptureStart = false;
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		hostAnim = GetComponent<Animator>();
 	}
 
 	private void FixedUpdate()
 	{
 		HostMove();
+		HostTurn();
 	}
 
 	#region Host Move Controller
@@ -36,10 +39,12 @@ public class HostController : MonoBehaviour
 			{
 				if (Input.GetKey(KeyCode.D))
 				{
+					hostAnim.SetBool("isWalk",true);
 					transform.Translate(Vector2.right * speed * Time.deltaTime);
 				}
 				else if (Input.GetKey(KeyCode.A))
 				{
+					hostAnim.SetBool("isWalk", true);
 					transform.Translate(Vector2.left * speed * Time.deltaTime);
 				}
 			}
@@ -51,6 +56,20 @@ public class HostController : MonoBehaviour
 		
 		}
 	
+	}
+	#endregion
+
+	#region Host Turn
+	void HostTurn()
+	{
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.localScale = new Vector2(1f, 1f);
+		}
+		else if(Input.GetKey(KeyCode.A))
+		{
+			transform.localScale = new Vector2(-1f, 1f);
+		}
 	}
 	#endregion
 }
